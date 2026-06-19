0c7e201073ca917332649e8b505940fcc947e2db
shinonome-MiDUki <shiinaayame3@gmail.com>
2026-06-19 23:19:16 +0900

    Implement cut-component intercept

--- python/shellarc_railgun/computation_core/intercepts/cut_component_plane.py
+++ python/shellarc_railgun/computation_core/intercepts/cut_component_plane.py
@@ -17,3 +17,15 @@ def get_diff_array(M: np.ndarray,
         ]
     )
     return combined_diff_array
+
+def get_frontend_array(M: np.ndarray,
+                       total_cut_num: int
+                       ) -> np.ndarray:
+    frontend_ls = []
+    for cut_num_i in total_cut_num:
+        cut_intercept = M[M[data_struct.NpData.POS][:, 0] == cut_num_i]
+        if len(cut_intercept) > 0:
+            frontend_ls.append(cut_intercept[data_struct.NpData.POS][:, 1].max())
+        else:
+            frontend_ls.append(0.0)
+    return np.array(frontend_ls)
